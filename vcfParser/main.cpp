1d4b6e26a8259edc9a6f38f33cdf70ed72548962
lalbrecht18 <albrecht.lili.adel@gmail.com>
2025-12-01 14:17:04 +0100

    commit

--- vcfParser/main.cpp
+++ vcfParser/main.cpp
@@ -4,7 +4,7 @@ using vcf::VCFParser;
 
 int main() {
 	// Example usage of VCFParser
-	VCFParser parser("vcfTestFileSimple.vcf");
+	VCFParser parser("1000genomes.vcf");	
 	vcf::VCFHeader h = parser.parseHeader();
 
 	return 0;
